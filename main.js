2c86df56d7535772ba820c081b00f226e7d65f7c
0verflowed-stack <spaydermendima@gmail.com>
2025-05-03 13:16:32 +0300

    Update startups carousel logic

--- main.js
+++ main.js
@@ -65,24 +65,25 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }).mount();
 
-  new Splide('#image-carousel-startups', {
-    type    : 'loop',
-    perPage : 5,
-    gap     : '1rem',
-    focus   : 2,
-    rewind  : true,
-    padding: '35px',
+  new Splide( '#image-carousel-startups', {
+    type       : 'loop',
+    perPage    : 3,
+    focus      : 'center',
+    gap        : '1rem',
+    arrows     : false,
+    padding    : '1rem',
     breakpoints: {
-      768: {
-        perPage: 1,
-        gap: '0.5rem',
-        focus: 0
-      },
       1200: {
-        perPage: 3,
-        gap: '0.5rem',
-        focus : 1,  
+        perPage : 3,
+        focus   : 'center',
+      },
+      768: {
+        type    : 'loop',
+        perPage : 1,
+        focus   : 0,
+        gap     : '0.5rem',
+        arrows     : true,
       }
     }
-  }).mount();
+  } ).mount();
 } );
